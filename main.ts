9eb9f8e87dc0ae59e35f5f025c2a284fdbc75f3a
Tim Osborn <tim@memelab.com.au>
2022-03-28 15:52:20 +1100

    Linting

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
-import {
-	addIcon, App, FileSystemAdapter, Plugin,
-	PluginSettingTab, Setting
-} from 'obsidian';
+import { App, FileSystemAdapter, Plugin, PluginSettingTab, Setting, addIcon } from 'obsidian';
 
 const svg = `
 <path
   fill="currentColor"
   d="M 96.453125 10.773438 L 75.882812 0.878906 C 73.488281 -0.277344 70.640625 0.210938 68.769531 2.082031 L 29.367188 38.035156 L 12.195312 25.011719 C 10.601562 23.789062 8.351562 23.890625 6.871094 25.242188 L 1.371094 30.253906 C -0.449219 31.898438 -0.449219 34.761719 1.355469 36.40625 L 16.25 49.996094 L 1.355469 63.585938 C -0.449219 65.230469 -0.449219 68.097656 1.371094 69.742188 L 6.871094 74.753906 C 8.367188 76.101562 10.601562 76.203125 12.195312 74.980469 L 29.367188 61.945312 L 68.789062 97.914062 C 70.644531 99.785156 73.492188 100.273438 75.882812 99.117188 L 96.476562 89.203125 C 98.640625 88.164062 100.007812 85.980469 100.007812 83.570312 L 100.007812 16.398438 C 100.007812 14.007812 98.621094 11.808594 96.460938 10.769531 Z M 75.015625 72.707031 L 45.101562 50 L 75.015625 27.292969 Z M 75.015625 72.707031"
-/>`
+/>`;
 
 addIcon('vscode-logo', svg);
 export default class OpenVSCode extends Plugin {
@@ -24,7 +21,7 @@ export default class OpenVSCode extends Plugin {
 		this.addCommand({
 			id: 'open-vscode',
 			name: 'Open as Visual Studio Code workspace',
-			callback: this.openVSCode.bind(this)
+			callback: this.openVSCode.bind(this),
 		});
 	}
 
@@ -59,8 +56,7 @@ export default class OpenVSCode extends Plugin {
 				console.log('[openVSCode]', { url });
 				window.open(url);
 			}, timeout);
-		}
-		else {
+		} else {
 			// eslint-disable-next-line @typescript-eslint/no-var-requires
 			const { exec } = require('child_process');
 
@@ -91,13 +87,13 @@ export default class OpenVSCode extends Plugin {
 				this.openVSCode();
 			});
 		}
-	}
+	};
 }
 
 interface OpenVSCodeSettings {
-	ribbonIcon: boolean,
-	useURL: boolean,
-	executeTemplate: string,
+	ribbonIcon: boolean;
+	useURL: boolean;
+	executeTemplate: string;
 	workspacePath: string;
 	openFile: boolean;
 }
@@ -108,10 +104,9 @@ const DEFAULT_SETTINGS: OpenVSCodeSettings = {
 	executeTemplate: 'code "{{vaultpath}}/{{filepath}}"',
 	workspacePath: '',
 	openFile: true,
-}
+};
 
 class OpenVSCodeSettingsTab extends PluginSettingTab {
-
 	plugin: OpenVSCode;
 
 	constructor(app: App, plugin: OpenVSCode) {
@@ -122,70 +117,71 @@ class OpenVSCodeSettingsTab extends PluginSettingTab {
 	display(): void {
 		const { containerEl } = this;
 		containerEl.empty();
-		containerEl.createEl("h2", { text: "Settings" });
+		containerEl.createEl('h2', { text: 'Settings' });
 
 		new Setting(containerEl)
 			.setName('Ribbon Icon')
 			.setDesc('Turn on if you want to have a Ribbon Icon.')
-			.addToggle((toggle) => toggle
-				.setValue(this.plugin.settings.ribbonIcon)
-				.onChange((value) => {
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.ribbonIcon).onChange((value) => {
 					this.plugin.settings.ribbonIcon = value;
 					this.plugin.saveSettings();
 					this.plugin.refreshIconRibbon();
-				})
+				}),
 			);
 		new Setting(containerEl)
 			.setName('Use URL')
 			.setDesc(
-				'Open VSCode using a `vscode://` URL instead of executing the `code` command. Opening via URL may be faster than the alternative on some systems.'
+				'Open VSCode using a `vscode://` URL instead of executing the `code` command. Opening via URL may be faster than the alternative on some systems.',
 			)
-			.addToggle((toggle) => toggle
-				.setValue(this.plugin.settings.useURL)
-				.onChange((value) => {
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.useURL).onChange((value) => {
 					this.plugin.settings.useURL = value;
 					this.plugin.saveSettings();
-				})
+				}),
 			);
 		new Setting(containerEl)
 			.setName('Default template for executing the `code` command')
 			.setDesc(
-				'You can use the following variables: `{{vaultpath}}`, `{{filepath}}` (relative). Note that on MacOS, a full path to the VSCode executable is required (generally "/usr/local/bin/code"). Example: `/usr/local/bin/code {{vaultpath}}/{{filepath}}`'
+				'You can use the following variables: `{{vaultpath}}`, `{{filepath}}` (relative). Note that on MacOS, a full path to the VSCode executable is required (generally "/usr/local/bin/code"). Example: `/usr/local/bin/code {{vaultpath}}/{{filepath}}`',
 			)
-			.addText(text => text.setPlaceholder(DEFAULT_SETTINGS.executeTemplate)
-				.setValue(this.plugin.settings.executeTemplate || DEFAULT_SETTINGS.executeTemplate)
-				.onChange((value) => {
-					value = value.trim();
-					if (value === "") value = DEFAULT_SETTINGS.executeTemplate;
-					this.plugin.settings.executeTemplate = value;
-					this.plugin.saveData(this.plugin.settings);
-				}));
+			.addText((text) =>
+				text
+					.setPlaceholder(DEFAULT_SETTINGS.executeTemplate)
+					.setValue(this.plugin.settings.executeTemplate || DEFAULT_SETTINGS.executeTemplate)
+					.onChange((value) => {
+						value = value.trim();
+						if (value === '') value = DEFAULT_SETTINGS.executeTemplate;
+						this.plugin.settings.executeTemplate = value;
+						this.plugin.saveData(this.plugin.settings);
+					}),
+			);
 		new Setting(containerEl)
-					.setName('Path to VSCode Workspace (Use URL only)')
-					.setDesc(
-						'If "Use URL" is checked, VSCode will open Obsidian files in this workspace (requires an absolute path)',
-					)
-					.addText((text) =>
-						text
-							.setPlaceholder(DEFAULT_SETTINGS.workspacePath)
-							.setValue(this.plugin.settings.workspacePath || DEFAULT_SETTINGS.workspacePath)
-							.onChange((value) => {
-								value = value.trim();
-								if (value === '') value = DEFAULT_SETTINGS.workspacePath;
-								this.plugin.settings.workspacePath = value;
-								this.plugin.saveData(this.plugin.settings);
-							}),
-					);
+			.setName('Path to VSCode Workspace (Use URL only)')
+			.setDesc(
+				'If "Use URL" is checked, VSCode will open Obsidian files in this workspace (requires an absolute path)',
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder(DEFAULT_SETTINGS.workspacePath)
+					.setValue(this.plugin.settings.workspacePath || DEFAULT_SETTINGS.workspacePath)
+					.onChange((value) => {
+						value = value.trim();
+						if (value === '') value = DEFAULT_SETTINGS.workspacePath;
+						this.plugin.settings.workspacePath = value;
+						this.plugin.saveData(this.plugin.settings);
+					}),
+			);
 
-				new Setting(containerEl)
-					.setName('Open current file (Use URL only)')
-					.setDesc('If "Use URL" is checked, open the current file rather than the root of the vault')
-					.addToggle((toggle) =>
-						toggle.setValue(this.plugin.settings.openFile || DEFAULT_SETTINGS.openFile).onChange((value) => {
-							this.plugin.settings.openFile = value;
-							this.plugin.saveData(this.plugin.settings);
-						}),
-					);
+		new Setting(containerEl)
+			.setName('Open current file (Use URL only)')
+			.setDesc('If "Use URL" is checked, open the current file rather than the root of the vault')
+			.addToggle((toggle) =>
+				toggle.setValue(this.plugin.settings.openFile || DEFAULT_SETTINGS.openFile).onChange((value) => {
+					this.plugin.settings.openFile = value;
+					this.plugin.saveData(this.plugin.settings);
+				}),
+			);
 	}
 }
 
