f01bdb0f95b867de8fa312ba98d409623f6bbe83
NomarCub <nomarcub@gmail.com>
2024-06-22 10:45:32 +0200

    update config, reformat with spaces

--- src/main.ts
+++ src/main.ts
@@ -5,179 +5,179 @@ import { DEFAULT_SETTINGS, OpenVSCodeSettings, OpenVSCodeSettingsTab } from "./s
 import { exec } from "child_process";
 
 type HotReloadPlugin = Plugin & {
-	// https://github.com/pjeby/hot-reload/blob/0.1.11/main.js#L70
-	enabledPlugins: Set<string>;
+    // https://github.com/pjeby/hot-reload/blob/0.1.11/main.js#L70
+    enabledPlugins: Set<string>;
 };
 
 export default class OpenVSCode extends Plugin {
-	static iconId = "vscode-logo";
-	// source: https://simpleicons.org/?q=visual-studio-code
-	static iconSvgContent = `
+    static iconId = "vscode-logo";
+    // source: https://simpleicons.org/?q=visual-studio-code
+    static iconSvgContent = `
 <svg role="img" viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg">
     <title>Visual Studio Code</title>
     <path
-		fill="currentColor"
-		d="M23.15 2.587L18.21.21a1.494 1.494 0 0 0-1.705.29l-9.46 8.63-4.12-3.128a.999.999 0 0 0-1.276.057L.327 7.261A1 1 0 0 0 .326 8.74L3.899 12 .326 15.26a1 1 0 0 0 .001 1.479L1.65 17.94a.999.999 0 0 0 1.276.057l4.12-3.128 9.46 8.63a1.492 1.492 0 0 0 1.704.29l4.942-2.377A1.5 1.5 0 0 0 24 20.06V3.939a1.5 1.5 0 0 0-.85-1.352zm-5.146 14.861L10.826 12l7.178-5.448v10.896z"
-	/>
+        fill="currentColor"
+        d="M23.15 2.587L18.21.21a1.494 1.494 0 0 0-1.705.29l-9.46 8.63-4.12-3.128a.999.999 0 0 0-1.276.057L.327 7.261A1 1 0 0 0 .326 8.74L3.899 12 .326 15.26a1 1 0 0 0 .001 1.479L1.65 17.94a.999.999 0 0 0 1.276.057l4.12-3.128 9.46 8.63a1.492 1.492 0 0 0 1.704.29l4.942-2.377A1.5 1.5 0 0 0 24 20.06V3.939a1.5 1.5 0 0 0-.85-1.352zm-5.146 14.861L10.826 12l7.178-5.448v10.896z"
+    />
 </svg>
 `;
 
-	DEV = false;
+    DEV = false;
 
-	ribbonIcon?: HTMLElement;
-	settings!: OpenVSCodeSettings;
+    ribbonIcon?: HTMLElement;
+    settings!: OpenVSCodeSettings;
 
-	override async onload() {
-		console.log("Loading " + this.manifest.name + " plugin");
-		addIcon(OpenVSCode.iconId, OpenVSCode.iconSvgContent);
-		await this.loadSettings();
-		this.refreshIconRibbon();
+    override async onload() {
+        console.log("Loading " + this.manifest.name + " plugin");
+        addIcon(OpenVSCode.iconId, OpenVSCode.iconSvgContent);
+        await this.loadSettings();
+        this.refreshIconRibbon();
 
-		this.addSettingTab(new OpenVSCodeSettingsTab(this.app, this));
+        this.addSettingTab(new OpenVSCodeSettingsTab(this.app, this));
 
-		this.addCommand({
-			id: "open-vscode",
-			name: "Open as Visual Studio Code workspace",
-			callback: this.openVSCode.bind(this),
-		});
+        this.addCommand({
+            id: "open-vscode",
+            name: "Open as Visual Studio Code workspace",
+            callback: this.openVSCode.bind(this),
+        });
 
-		this.addCommand({
-			id: "open-vscode-via-url",
-			name: "Open as Visual Studio Code workspace using a vscode:// URL",
-			callback: this.openVSCodeUrl.bind(this),
-		});
+        this.addCommand({
+            id: "open-vscode-via-url",
+            name: "Open as Visual Studio Code workspace using a vscode:// URL",
+            callback: this.openVSCodeUrl.bind(this),
+        });
 
-		const hotReloadPlugin = this.app.plugins.getPlugin("hot-reload") as HotReloadPlugin | null;
-		this.DEV = hotReloadPlugin?.enabledPlugins.has(this.manifest.id) ?? false;
+        const hotReloadPlugin = this.app.plugins.getPlugin("hot-reload") as HotReloadPlugin | null;
+        this.DEV = hotReloadPlugin?.enabledPlugins.has(this.manifest.id) ?? false;
 
-		if (this.DEV) {
-			this.addCommand({
-				id: "open-vscode-reload",
-				name: "Reload the plugin in dev",
-				callback: this.reload.bind(this),
-			});
+        if (this.DEV) {
+            this.addCommand({
+                id: "open-vscode-reload",
+                name: "Reload the plugin in dev",
+                callback: this.reload.bind(this),
+            });
 
-			this.addCommand({
-				id: "open-vscode-reset-settings",
-				name: "Reset plugins settings to default in dev",
-				callback: this.resetSettings.bind(this),
-			});
-		}
-	}
+            this.addCommand({
+                id: "open-vscode-reset-settings",
+                name: "Reset plugins settings to default in dev",
+                callback: this.resetSettings.bind(this),
+            });
+        }
+    }
 
-	openVSCode() {
-		if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
-			return;
-		}
-		const { executeTemplate } = this.settings;
+    openVSCode() {
+        if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
+            return;
+        }
+        const { executeTemplate } = this.settings;
 
-		const path = this.app.vault.adapter.getBasePath();
-		const file = this.app.workspace.getActiveFile();
-		const filePath = file?.path ?? "";
-		const folderPath = file?.parent?.path ?? "";
+        const path = this.app.vault.adapter.getBasePath();
+        const file = this.app.workspace.getActiveFile();
+        const filePath = file?.path ?? "";
+        const folderPath = file?.parent?.path ?? "";
 
-		let command = executeTemplate.trim() === "" ? DEFAULT_SETTINGS.executeTemplate : executeTemplate;
-		command = command
-			.replaceAll("{{vaultpath}}", path)
-			.replaceAll("{{filepath}}", filePath)
-			.replaceAll("{{folderpath}}", folderPath);
-		if (this.DEV) console.log("[openVSCode]", { command });
-		exec(command, error => {
-			if (error) {
-				console.error(`[openVSCode] exec error: ${error.message}`);
-			}
-		});
-	}
+        let command = executeTemplate.trim() === "" ? DEFAULT_SETTINGS.executeTemplate : executeTemplate;
+        command = command
+            .replaceAll("{{vaultpath}}", path)
+            .replaceAll("{{filepath}}", filePath)
+            .replaceAll("{{folderpath}}", folderPath);
+        if (this.DEV) console.log("[openVSCode]", { command });
+        exec(command, error => {
+            if (error) {
+                console.error(`[openVSCode] exec error: ${error.message}`);
+            }
+        });
+    }
 
-	openVSCodeUrl() {
-		if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
-			return;
-		}
-		const { openFile, useUrlInsiders } = this.settings;
+    openVSCodeUrl() {
+        if (!(this.app.vault.adapter instanceof FileSystemAdapter)) {
+            return;
+        }
+        const { openFile, useUrlInsiders } = this.settings;
 
-		const path = this.app.vault.adapter.getBasePath();
-		const file = this.app.workspace.getActiveFile();
-		const filePath = file?.path ?? "";
-		if (this.DEV)
-			console.log("[open-vscode]", {
-				settings: this.settings,
-				path,
-				filePath,
-			});
+        const path = this.app.vault.adapter.getBasePath();
+        const file = this.app.workspace.getActiveFile();
+        const filePath = file?.path ?? "";
+        if (this.DEV)
+            console.log("[open-vscode]", {
+                settings: this.settings,
+                path,
+                filePath,
+            });
 
-		// https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
-		const protocol = useUrlInsiders ? "vscode-insiders://" : "vscode://";
-		let url = `${protocol}file/${path}`;
+        // https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
+        const protocol = useUrlInsiders ? "vscode-insiders://" : "vscode://";
+        let url = `${protocol}file/${path}`;
 
-		if (openFile) {
-			url += `/${filePath}`;
-			/*
-			By default, opening a file via the vscode:// URL will cause that file to open
-			in the front-most window in VSCode. We assume that it is preferred that files from
-			Obsidian should all open in the same workspace.
+        if (openFile) {
+            url += `/${filePath}`;
+            /*
+            By default, opening a file via the vscode:// URL will cause that file to open
+            in the front-most window in VSCode. We assume that it is preferred that files from
+            Obsidian should all open in the same workspace.
 
-			As a workaround, we issue two open requests to VSCode in quick succession: the first to
-			bring the workspace to front, the second to open the file.
+            As a workaround, we issue two open requests to VSCode in quick succession: the first to
+            bring the workspace to front, the second to open the file.
 
-			There is a ticket requesting this feature for VSCode:
-			https://github.com/microsoft/vscode/issues/150078
-			*/
+            There is a ticket requesting this feature for VSCode:
+            https://github.com/microsoft/vscode/issues/150078
+            */
 
-			// HACK: first open the _workspace_ to bring the correct window to the front....
-			const workspacePath = this.settings.workspacePath.replaceAll("{{vaultpath}}", path);
-			window.open(`vscode://file/${workspacePath}`);
+            // HACK: first open the _workspace_ to bring the correct window to the front....
+            const workspacePath = this.settings.workspacePath.replaceAll("{{vaultpath}}", path);
+            window.open(`vscode://file/${workspacePath}`);
 
-			// ...then open the _file_ in a setTimeout callback to allow time for the workspace to be activated
-			setTimeout(() => {
-				if (this.DEV) console.log("[openVSCode]", { url });
-				window.open(url);
-			}, 200); // anecdotally, this seems to be the min required for the workspace to activate
-		} else {
-			if (this.DEV) console.log("[openVSCode]", { url });
-			window.open(url);
-		}
-	}
+            // ...then open the _file_ in a setTimeout callback to allow time for the workspace to be activated
+            setTimeout(() => {
+                if (this.DEV) console.log("[openVSCode]", { url });
+                window.open(url);
+            }, 200); // anecdotally, this seems to be the min required for the workspace to activate
+        } else {
+            if (this.DEV) console.log("[openVSCode]", { url });
+            window.open(url);
+        }
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()) as OpenVSCodeSettings;
-	}
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData()) as OpenVSCodeSettings;
+    }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-	refreshIconRibbon = () => {
-		this.ribbonIcon?.remove();
-		if (this.settings.ribbonIcon) {
-			this.ribbonIcon = this.addRibbonIcon(OpenVSCode.iconId, "VSCode", () => {
-				const ribbonCommand = this.settings.ribbonCommandUsesCode ? "openVSCode" : "openVSCodeUrl";
-				this[ribbonCommand]();
-			});
-		}
-	};
+    refreshIconRibbon = () => {
+        this.ribbonIcon?.remove();
+        if (this.settings.ribbonIcon) {
+            this.ribbonIcon = this.addRibbonIcon(OpenVSCode.iconId, "VSCode", () => {
+                const ribbonCommand = this.settings.ribbonCommandUsesCode ? "openVSCode" : "openVSCodeUrl";
+                this[ribbonCommand]();
+            });
+        }
+    };
 
-	/**
-	 * [pjeby](https://forum.obsidian.md/t/how-to-get-started-with-developing-a-custom-plugin/8157/7)
-	 *
-	 * > ...while doing development, you may need to reload your plugin
-	 * > after making changes. You can do this by reloading, sure, but it’s easier
-	 * > to just go to settings and then toggle the plugin off, then back on again.
-	 * >
-	 * > You can also automate this process from within the plugin itself, by
-	 * > including a command that does something like this:
-	 */
-	async reload() {
-		const id = this.manifest.id;
-		const plugins = this.app.plugins;
-		await plugins.disablePlugin(id);
-		await plugins.enablePlugin(id);
-		console.log(`[${this.manifest.id}] reloaded`, this);
-	}
+    /**
+     * [pjeby](https://forum.obsidian.md/t/how-to-get-started-with-developing-a-custom-plugin/8157/7)
+     *
+     * > ...while doing development, you may need to reload your plugin
+     * > after making changes. You can do this by reloading, sure, but it’s easier
+     * > to just go to settings and then toggle the plugin off, then back on again.
+     * >
+     * > You can also automate this process from within the plugin itself, by
+     * > including a command that does something like this:
+     */
+    async reload() {
+        const id = this.manifest.id;
+        const plugins = this.app.plugins;
+        await plugins.disablePlugin(id);
+        await plugins.enablePlugin(id);
+        console.log(`[${this.manifest.id}] reloaded`, this);
+    }
 
-	async resetSettings() {
-		console.log(`[${this.manifest.id}]`, { old: this.settings, default: DEFAULT_SETTINGS });
-		this.settings = DEFAULT_SETTINGS;
-		await this.saveData(this.settings);
-	}
+    async resetSettings() {
+        console.log(`[${this.manifest.id}]`, { old: this.settings, default: DEFAULT_SETTINGS });
+        this.settings = DEFAULT_SETTINGS;
+        await this.saveData(this.settings);
+    }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -2,130 +2,129 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 import OpenVSCode from "./main";
 
 export interface OpenVSCodeSettings {
-	ribbonIcon: boolean;
-	ribbonCommandUsesCode: boolean;
-	executeTemplate: string;
-	openFile: boolean;
-	workspacePath: string;
-	useUrlInsiders: boolean;
+    ribbonIcon: boolean;
+    ribbonCommandUsesCode: boolean;
+    executeTemplate: string;
+    openFile: boolean;
+    workspacePath: string;
+    useUrlInsiders: boolean;
 }
 
 export const DEFAULT_SETTINGS: OpenVSCodeSettings = {
-	ribbonIcon: true,
-	ribbonCommandUsesCode: true,
-	executeTemplate: 'code "{{vaultpath}}" "{{vaultpath}}/{{filepath}}"',
-	openFile: true,
-	workspacePath: "{{vaultpath}}",
-	useUrlInsiders: false,
+    ribbonIcon: true,
+    ribbonCommandUsesCode: true,
+    executeTemplate: 'code "{{vaultpath}}" "{{vaultpath}}/{{filepath}}"',
+    openFile: true,
+    workspacePath: "{{vaultpath}}",
+    useUrlInsiders: false,
 };
 
 export class OpenVSCodeSettingsTab extends PluginSettingTab {
-	override plugin: OpenVSCode;
+    override plugin: OpenVSCode;
 
-	constructor(app: App, plugin: OpenVSCode) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(app: App, plugin: OpenVSCode) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	override display(): void {
-		const { containerEl } = this;
-		containerEl.empty();
-		containerEl.createEl("h3", { text: "General settings" });
+    override display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
+        containerEl.createEl("h3", { text: "General settings" });
 
-		new Setting(containerEl)
-			.setName("Display Ribbon Icon")
-			.setDesc("Toggle this OFF if you want to hide the Ribbon Icon.")
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.ribbonIcon)
-				.onChange(value => {
-					this.plugin.settings.ribbonIcon = value;
-					void this.plugin.saveSettings();
-					this.plugin.refreshIconRibbon();
-				}),
-			);
+        new Setting(containerEl)
+            .setName("Display Ribbon Icon")
+            .setDesc("Toggle this OFF if you want to hide the Ribbon Icon.")
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.ribbonIcon)
+                .onChange(value => {
+                    this.plugin.settings.ribbonIcon = value;
+                    void this.plugin.saveSettings();
+                    this.plugin.refreshIconRibbon();
+                }),
+            );
 
-		new Setting(containerEl)
-			.setName("Ribbon opens via `code` command")
-			.setDesc("Toggle this OFF if you'd prefer that the Ribbon Icon opens VSCode via URL.")
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.ribbonCommandUsesCode)
-				.onChange(value => {
-					this.plugin.settings.ribbonCommandUsesCode = value;
-					void this.plugin.saveSettings();
-				}),
-			);
+        new Setting(containerEl)
+            .setName("Ribbon opens via `code` command")
+            .setDesc("Toggle this OFF if you'd prefer that the Ribbon Icon opens VSCode via URL.")
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.ribbonCommandUsesCode)
+                .onChange(value => {
+                    this.plugin.settings.ribbonCommandUsesCode = value;
+                    void this.plugin.saveSettings();
+                }),
+            );
 
-		containerEl.createEl("h3", { text: "Open via `code` CLI settings" });
+        containerEl.createEl("h3", { text: "Open via `code` CLI settings" });
 
-		new Setting(containerEl)
-			.setName("Template for executing the `code` command")
-			.setDesc('You can use the following variables: `{{vaultpath}}` (absolute), `{{filepath}}` (relative), `{{folderpath}}` (relative). Note that on MacOS, a full path to the VSCode executable is required (generally "/usr/local/bin/code"). Example: `/usr/local/bin/code "{{vaultpath}}" "{{vaultpath}}/{{filepath}}"`')
-			.addText(text => text
-				.setPlaceholder(DEFAULT_SETTINGS.executeTemplate)
-				.setValue(this.plugin.settings.executeTemplate || DEFAULT_SETTINGS.executeTemplate)
-				.onChange(value => {
-					value = value.trim();
-					if (value === "") value = DEFAULT_SETTINGS.executeTemplate;
-					this.plugin.settings.executeTemplate = value;
-					void this.plugin.saveData(this.plugin.settings);
-				}),
-			);
+        new Setting(containerEl)
+            .setName("Template for executing the `code` command")
+            .setDesc('You can use the following variables: `{{vaultpath}}` (absolute), `{{filepath}}` (relative), `{{folderpath}}` (relative). Note that on MacOS, a full path to the VSCode executable is required (generally "/usr/local/bin/code"). Example: `/usr/local/bin/code "{{vaultpath}}" "{{vaultpath}}/{{filepath}}"`')
+            .addText(text => text
+                .setPlaceholder(DEFAULT_SETTINGS.executeTemplate)
+                .setValue(this.plugin.settings.executeTemplate || DEFAULT_SETTINGS.executeTemplate)
+                .onChange(value => {
+                    value = value.trim();
+                    if (value === "") value = DEFAULT_SETTINGS.executeTemplate;
+                    this.plugin.settings.executeTemplate = value;
+                    void this.plugin.saveData(this.plugin.settings);
+                }),
+            );
 
-		containerEl.createEl("h3", { text: "Open via `vscode://` URL settings" });
+        containerEl.createEl("h3", { text: "Open via `vscode://` URL settings" });
 
-		const openViaUrlCaveat = containerEl.createEl("p");
-		const openViaUrlCaveatEm = openViaUrlCaveat.createEl("em", {
-			text: `⚠️ This setting is not recommended for Windows users due to
-				UX issues caused by security enhancements in VSCode on Windows.
-				More information: `,
-		});
-		openViaUrlCaveatEm.appendChild(createEl("a", {
-			text: "Open in VSCode Readme",
-			href: "https://github.com/NomarCub/obsidian-open-vscode/blob/master/README.md#caveats-regarding-the-url-command-for-windows-users",
-		}));
-		openViaUrlCaveatEm.appendText(".");
+        const openViaUrlCaveat = containerEl.createEl("p");
+        const openViaUrlCaveatEm = openViaUrlCaveat.createEl("em", {
+            text: `⚠️ This setting is not recommended for Windows users due to
+              UX issues caused by security enhancements in VSCode on Windows. More information: `,
+        });
+        openViaUrlCaveatEm.appendChild(createEl("a", {
+            text: "Open in VSCode Readme",
+            href: "https://github.com/NomarCub/obsidian-open-vscode/blob/master/README.md#caveats-regarding-the-url-command-for-windows-users",
+        }));
+        openViaUrlCaveatEm.appendText(".");
 
-		new Setting(containerEl)
-			.setName("Open current file")
-			.setDesc("Open the current file rather than the root of the vault.")
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.openFile || DEFAULT_SETTINGS.openFile)
-				.onChange(value => {
-					this.plugin.settings.openFile = value;
-					void this.plugin.saveData(this.plugin.settings);
-				}),
-			);
+        new Setting(containerEl)
+            .setName("Open current file")
+            .setDesc("Open the current file rather than the root of the vault.")
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.openFile || DEFAULT_SETTINGS.openFile)
+                .onChange(value => {
+                    this.plugin.settings.openFile = value;
+                    void this.plugin.saveData(this.plugin.settings);
+                }),
+            );
 
-		const workspacePathSetting = new Setting(containerEl)
-			.setName("Path to VSCode Workspace")
-			.setDesc('Defaults to the {{vaultpath}} template variable. You can set this to an absolute path to a ".code-workspace" file if you prefer to use a Multi Root workspace file: ')
-			.addText(text => text
-				.setPlaceholder(DEFAULT_SETTINGS.workspacePath)
-				.setValue(this.plugin.settings.workspacePath || DEFAULT_SETTINGS.workspacePath)
-				.onChange(value => {
-					value = value.trim();
-					if (value === "") value = DEFAULT_SETTINGS.workspacePath;
-					this.plugin.settings.workspacePath = value;
-					void this.plugin.saveData(this.plugin.settings);
-				}),
-			);
+        const workspacePathSetting = new Setting(containerEl)
+            .setName("Path to VSCode Workspace")
+            .setDesc('Defaults to the {{vaultpath}} template variable. You can set this to an absolute path to a ".code-workspace" file if you prefer to use a Multi Root workspace file: ')
+            .addText(text => text
+                .setPlaceholder(DEFAULT_SETTINGS.workspacePath)
+                .setValue(this.plugin.settings.workspacePath || DEFAULT_SETTINGS.workspacePath)
+                .onChange(value => {
+                    value = value.trim();
+                    if (value === "") value = DEFAULT_SETTINGS.workspacePath;
+                    this.plugin.settings.workspacePath = value;
+                    void this.plugin.saveData(this.plugin.settings);
+                }),
+            );
 
-		const multiRootWorkspacesLink = "https://code.visualstudio.com/docs/editor/workspaces#_multiroot-workspaces";
-		workspacePathSetting.descEl
-			.appendChild(createEl("a", {
-				href: multiRootWorkspacesLink,
-				text: multiRootWorkspacesLink,
-			}))
-			.appendText(".");
+        const multiRootWorkspacesLink = "https://code.visualstudio.com/docs/editor/workspaces#_multiroot-workspaces";
+        workspacePathSetting.descEl
+            .appendChild(createEl("a", {
+                href: multiRootWorkspacesLink,
+                text: multiRootWorkspacesLink,
+            }))
+            .appendText(".");
 
-		new Setting(containerEl)
-			.setName("Open VSCode using a `vscode-insiders://` URL")
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.useUrlInsiders)
-				.onChange(value => {
-					this.plugin.settings.useUrlInsiders = value;
-					void this.plugin.saveSettings();
-				})
-			);
-	}
+        new Setting(containerEl)
+            .setName("Open VSCode using a `vscode-insiders://` URL")
+            .addToggle(toggle => toggle
+                .setValue(this.plugin.settings.useUrlInsiders)
+                .onChange(value => {
+                    this.plugin.settings.useUrlInsiders = value;
+                    void this.plugin.saveSettings();
+                })
+            );
+    }
 }
