b34dd36d79980332c31ff71434e5cc06986a0ef0
NomarCub <nomarcub@gmail.com>
2026-04-01 20:44:32 +0200

    update ESLint config to include Obsidian lint plugin - based on: <https://github.com/obsidianmd/eslint-plugin/issues/90>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import eslint from "@eslint/js";
+import obsidianmd from "eslint-plugin-obsidianmd";
 import tseslint from "typescript-eslint";
 
 /* eslint-disable-next-line @typescript-eslint/no-deprecated --
@@ -10,10 +11,14 @@ export default tseslint.config({
         tseslint.configs.strictTypeChecked,
         tseslint.configs.stylisticTypeChecked,
     ],
+    plugins: { obsidianmd },
     languageOptions: {
         parserOptions: { projectService: true },
     },
+    // @ts-expect-error -- Temporary fixes for v0.1.9., see https://github.com/obsidianmd/eslint-plugin/issues/90
     rules: {
+        // eslint-disable-next-line @typescript-eslint/no-misused-spread 
+        ...obsidianmd.configs?.["recommended"],
         "@typescript-eslint/explicit-function-return-type": ["error", { allowExpressions: true }],
         "@typescript-eslint/restrict-template-expressions": ["error", { allowNumber: true }],
         "@typescript-eslint/no-non-null-assertion": "off",
